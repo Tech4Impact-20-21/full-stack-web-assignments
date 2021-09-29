2809419c949e471c7e4478a956192384772b9adb
MervinWin <90672068+MervinWin@users.noreply.github.com>
2021-09-29 13:25:29 +0700

    Add files via upload

---
+++ 06-JavaScript-Dasar/03-condition/03-condition/Soal-01-condition.js
@@ -0,0 +1,24 @@
+/// Soal - 01
+/// Buatlah sebuah kondisi dimana akan melakukan pengecekan terhadap user yang hendak bepergian menggunakan motor, ada beberapa hal yang perlu dilakukan salah satu-nya adalah
+/// - kondisi dibuat menggunakn IF ELSE
+/// - kondisi pertama akan mengecek apakah user sudah berusia kurang dari 18 tahun
+/// - jika IYA, maka cetak tulisan "Anda belum cukup umur"
+/// - jika TIDAK
+///   buatlah kondisi lagi dimana akan melakukan cek terhadap user tersebut, apakah dia sudah memiliki SIM atau belum
+///   - jika IYA, maka cetak tulisan "Selamat berkendara"
+///   - jika Tidak, maka cetak tulisan "Diharapkan untuk membuat SIM terlebih dahulu"
+
+const userAge = 18;
+const drivingLicense = true;
+
+if (userAge < 18){
+    console.info("Anda belum cukup umur");
+}
+else{
+    if (drivingLicense === true){
+        console.log("Selamat Berkendara");
+    }
+    else{
+        console.info("Diharapkan untuk membuat SIM terlebih dahulu");
+    }
+}
