80988df8f63861fbf108a942df68635d24ee6c9c
MervinWin <90672068+MervinWin@users.noreply.github.com>
2021-09-29 13:24:29 +0700

    Add files via upload

---
+++ 06-JavaScript-Dasar/01-data-type/01-data-type/Soal-05-data-type.js
@@ -0,0 +1,8 @@
+const name = "skilvul"; /// String
+const age = 10; /// Number
+const isMarried = false; /// Boolean
+
+/// Soal - 05
+/// Gunakan built-in function yang dimiliki oleh Javascript untuk mengubah sebuah String menjadi Number
+const stringToNumber = "1000";
+console.log(parseInt(stringToNumber));
